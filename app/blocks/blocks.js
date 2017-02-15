55172eca3c6d3ef49159336e1bc2b8bd5939172f
mesh00 <mesh44380@gmail.com>
2017-02-15 04:13:45 +0000

    better negative luck

--- app/blocks/blocks.js
+++ app/blocks/blocks.js
@@ -18,14 +18,13 @@ app.controller('BlocksCtrl', function($scope, $route, dataService, timerService)
 				$scope.blocks.global[index].maturity = $scope.config.maturity_depth - ($scope.network.height - block.height);
 
 				luck = block.shares/block.diff*100;
-				$scope.blocks.global[index].luck = (luck <= 100) ? (100-luck) : -luck ;
+				$scope.blocks.global[index].luck = (luck <= 100) ? (100-luck) : (-luck+100) ;
 				$scope.blocks.global[index].icon = (block.valid) ? 'done' : 'clear';
 			}
 		});
 	}
 
 	$scope.$watchGroup(["blocks.global", "poolStats.global"], updateMaturity);
-
 
 	// Register call with timer 
 	timerService.register(loadData, $route.current.controller);
