44b174ed1da3ee900f026e6b983519929fa3ac69
mesh00 <mesh44380@gmail.com>
2017-02-21 07:09:38 +0000

    pagination for blocks and payment page

--- app/payments/payments.js
+++ app/payments/payments.js
@@ -4,15 +4,17 @@ app.controller('PaymentsCtrl', function($scope, dataService) {
 	$scope.payments = {};
 	$scope.selected = [];
 
-	// dataService.getData("/pool/payments/pplns", function(data){
-	// 	$scope.payments.pplns = data;
-	// });
+	$scope.options = {
+		page: 1,
+		limit: 15
+	}
 
-	// dataService.getData("/pool/payments/pps", function(data){
-	// 	$scope.payments.pps = data;
-	// });
+	$scope.loadPayments = function () {
+		dataService.getData("/pool/payments?"+$.param($scope.options), function(data){
+			$scope.payments.global = data;
+		});
+	}
 
-    $scope.promise = dataService.getData("/pool/payments", function(data){
-        $scope.payments.global = data;
-    });
+	$scope.loadPayments();
+
 });
