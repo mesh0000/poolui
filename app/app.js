3103a8fa6858ee9b6c963c96fffc89df3090d3e4
mesh00 <mesh44380@gmail.com>
2017-04-13 02:02:40 +0100

    cache update fix

--- app/app.js
+++ app/app.js
@@ -15,7 +15,8 @@ var app = angular.module('poolui', [
 	'n3-line-chart',
 	'angular-page-visibility'
 	]).config(['$locationProvider', '$routeProvider', '$mdThemingProvider', function($locationProvider, $routeProvider, $mdThemingProvider) {
-		$locationProvider.hashPrefix('');	
+		$locationProvider.hashPrefix('')
+		;	
 		$mdThemingProvider.theme('default')
 		.primaryPalette('grey')
 		.accentPalette('light-blue');
@@ -77,153 +78,153 @@ var app = angular.module('poolui', [
 		$scope.$storage = $localStorage;
 
 		$scope.poolList = ["pplns", "pps", "solo"];
-	$scope.poolStats = {}; // All Pool stats
-	$scope.addrStats = {}; // All tracked addresses
-	$scope.lastBlock = {};
-	
-	// for miner tracking
-	$scope.yourTotalHashRate = 0;
-
-	// Hashrate Alarm
-	$scope.globalSiren = false;
-	$scope.sirenAudio = ngAudio.load("assets/ding.wav");
-	
-	// Update global hashrate and set off alarm if any of the tracked addresses fall below the threshold
-	var updateHashRate = function (addrStats){
-		var totalHashRate = 0;
-		var siren = false;
+		$scope.poolStats = {}; // All Pool stats
+		$scope.addrStats = {}; // All tracked addresses
+		$scope.lastBlock = {};
 		
-		_.each(addrStats, function(addr,index) {
-			totalHashRate += addr.hash;
-			if (addr.alarm && addr.hash < addr.alarmLimit) {
-				siren=true;
-			}
-		});
+		// for miner tracking
+		$scope.yourTotalHashRate = 0;
 
-		$scope.globalSiren=siren;
-		$scope.yourTotalHashRate = totalHashRate;
-	}
+		// Hashrate Alarm
+		$scope.globalSiren = false;
+		$scope.sirenAudio = ngAudio.load("assets/ding.wav");
+		
+		// Update global hashrate and set off alarm if any of the tracked addresses fall below the threshold
+		var updateHashRate = function (addrStats){
+			var totalHashRate = 0;
+			var siren = false;
+			
+			_.each(addrStats, function(addr,index) {
+				totalHashRate += addr.hash;
+				if (addr.alarm && addr.hash < addr.alarmLimit) {
+					siren=true;
+				}
+			});
 
-	var playSiren = function (){
-		($scope.globalSiren) ? $scope.sirenAudio.play() : $scope.sirenAudio.stop();
-	}
+			$scope.globalSiren=siren;
+			$scope.yourTotalHashRate = totalHashRate;
+		}
 
-	// ------- UI HELPERS
+		var playSiren = function (){
+			($scope.globalSiren) ? $scope.sirenAudio.play() : $scope.sirenAudio.stop();
+		}
 
-	$scope.menuOpen = $mdMedia('gt-md');
-	$scope.$watch(function() { return $mdMedia('gt-md'); }, function(big) {
+		// ------- UI HELPERS
+
 		$scope.menuOpen = $mdMedia('gt-md');
-	});
-
-	$scope.toggleSidenav = function (){
-		if (!$mdMedia('gt-md')) {
-			$mdSidenav('left').toggle();
-		} else {
-			// toggle boolean
-			$scope.menuOpen = !$scope.menuOpen;
-		}
-	}
-
-	// ------- Miner Login and auth
-	$scope.minerLogin = function (ev) {
-		$mdDialog.show({
-			controller: "LoginCtrl",
-			templateUrl: 'user/home/login.html',
-			parent: angular.element(document.body),
-			targetEvent: ev,
-			clickOutsideToClose:true,
-		  fullscreen: !$scope.menuOpen // Only for -xs, -sm breakpoints.
-		})
-		.then(function(answer) {
-		  // success callback
-		}, function(error) {
-		  // error callback
+		$scope.$watch(function() { return $mdMedia('gt-md'); }, function(big) {
+			$scope.menuOpen = $mdMedia('gt-md');
 		});
-	}
 
-	$scope.minerConsole = function (ev) {
-		$mdDialog.show({
-			locals: $scope.config,
-			controller: "ConsoleCtrl",
-			templateUrl: 'user/home/console.html',
-			parent: angular.element(document.body),
-			targetEvent: ev,
-			clickOutsideToClose:true,
-		  fullscreen: !$scope.menuOpen // Only for -xs, -sm breakpoints.
-		})
-		.then(function(answer){
-			if(answer=='logout'){
-				dataService.logout();
+		$scope.toggleSidenav = function (){
+			if (!$mdMedia('gt-md')) {
+				$mdSidenav('left').toggle();
+			} else {
+				// toggle boolean
+				$scope.menuOpen = !$scope.menuOpen;
 			}
-		}, function(reason){
-			// console.log(reason);
-		});
-	}
-
-	$scope.isLoggedIn = function() {
-		return dataService.isLoggedIn();
-	}
-
-	// ------- App Update
-	var update = function() {
-		if (appCache.status == window.applicationCache.UPDATEREADY) {
-			$window.location.reload();
 		}
-	}
 
-	appCache.addEventListener("updateready", function(event) {
+		// ------- Miner Login and auth
+		$scope.minerLogin = function (ev) {
+			$mdDialog.show({
+				controller: "LoginCtrl",
+				templateUrl: 'user/home/login.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose:true,
+			  fullscreen: !$scope.menuOpen // Only for -xs, -sm breakpoints.
+			})
+			.then(function(answer) {
+			  // success callback
+			}, function(error) {
+			  // error callback
+			});
+		}
+
+		$scope.minerConsole = function (ev) {
+			$mdDialog.show({
+				locals: $scope.config,
+				controller: "ConsoleCtrl",
+				templateUrl: 'user/home/console.html',
+				parent: angular.element(document.body),
+				targetEvent: ev,
+				clickOutsideToClose:true,
+			  fullscreen: !$scope.menuOpen // Only for -xs, -sm breakpoints.
+			})
+			.then(function(answer){
+				if(answer=='logout'){
+					dataService.logout();
+				}
+			}, function(reason){
+				// console.log(reason);
+			});
+		}
+
+		$scope.isLoggedIn = function() {
+			return dataService.isLoggedIn();
+		}
+
+		// ------- App Update
+		var update = function() {
+			if (appCache.status == window.applicationCache.UPDATEREADY) {
+				appCache.swapCache(); 
+				$window.location.reload();
+			}
+		}
+
+		appCache.addEventListener("updateready", function(event) {
+			update();
+		}, false);
+
+		var updateCache = function () {
+			appCache.update();
+		}
+
+		// API Requests
+		var loadData = function () {
+			dataService.getData("/pool/stats", function(data){
+				$scope.poolList = data.pool_list;
+				$scope.poolStats.global = data.pool_statistics;
+			});
+
+			dataService.getData("/network/stats", function(data){
+				$scope.network = data;
+			});	
+		}
+
+		var loadOnce = function () {
+			dataService.getData("/config", function(data){
+				$scope.config = data;
+			});
+		}
+
+		// For FAQ
+		$rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+			$location.hash($routeParams.scrollTo);
+			$anchorScroll();  
+		});
+
+		// Start doing things
+		loadOnce();
+		loadData();
 		update();
-	}, false);
+		
+		// Start the timer and register global requests
+		timerService.startTimer(GLOBALS.api_refresh_interval);
+		timerService.register(loadData, 'global');
+		$interval(updateCache, GLOBALS.app_update_interval); // check for app updates every 5 mins
 
-	var updateCache = function () {
-		var appCache = window.applicationCache;
-		appCache.update();
-	}
+		// Start address tracking servuce after starting timer, only one callback supported at a time
+		addressService.start(function(addrStats) {
+			$scope.addrStats = addrStats;
+			updateHashRate(addrStats);
+			playSiren();
+		}
+		);
 
-	// API Requests
-	var loadData = function () {
-		dataService.getData("/pool/stats", function(data){
-			$scope.poolList = data.pool_list;
-			$scope.poolStats.global = data.pool_statistics;
-		});
 
-		dataService.getData("/network/stats", function(data){
-			$scope.network = data;
-		});	
-	}
+		// Sponsor
+		$scope.sponsor_open = false
 
-	var loadOnce = function () {
-		dataService.getData("/config", function(data){
-			$scope.config = data;
-		});
-	}
-
-	// For FAQ
-	$rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
-		$location.hash($routeParams.scrollTo);
-		$anchorScroll();  
 	});
-
-	// Start doing things
-	loadOnce();
-	loadData();
-	updateCache();
-	
-	// Start the timer and register global requests
-	timerService.startTimer(GLOBALS.api_refresh_interval);
-	timerService.register(loadData, 'global');
-	$interval(updateCache, GLOBALS.app_update_interval); // check for app updates every 5 mins
-
-	// Start address tracking servuce after starting timer, only one callback supported at a time
-	addressService.start(function(addrStats) {
-		$scope.addrStats = addrStats;
-		updateHashRate(addrStats);
-		playSiren();
-	}
-	);
-
-
-	// Sponsor
-	$scope.sponsor_open = false
-
-});
