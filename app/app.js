1d6cb30c8e19c5d703e8219b0c8cad4f9b2be0b1
mesh00 <mesh44380@gmail.com>
2017-04-13 01:54:23 +0100

    revert default route

--- app/app.js
+++ app/app.js
@@ -15,8 +15,7 @@ var app = angular.module('poolui', [
 	'n3-line-chart',
 	'angular-page-visibility'
 	]).config(['$locationProvider', '$routeProvider', '$mdThemingProvider', function($locationProvider, $routeProvider, $mdThemingProvider) {
-		$locationProvider.hashPrefix('')
-		;	
+		$locationProvider.hashPrefix('');	
 		$mdThemingProvider.theme('default')
 		.primaryPalette('grey')
 		.accentPalette('light-blue');
@@ -68,7 +67,7 @@ var app = angular.module('poolui', [
 			activetab: 'help'
 		});
 
-
+		$routeProvider.otherwise({redirectTo: '/home'});
 
 	}]);
 
